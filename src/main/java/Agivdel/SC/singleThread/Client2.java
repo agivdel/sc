adba253b59c5e0c666e74040297b11e4d58d802c
agivdel <agph@yandex.ru>
2020-06-22 15:47:18 +0300

    чтение с консоли и отправка на сервер - в потоке main

--- src/main/java/Agivdel/SC/singleThread/Client2.java
+++ src/main/java/Agivdel/SC/singleThread/Client2.java
@@ -9,64 +9,41 @@ public class Client2 {
     private static DataInputStream in;//вариант 2
     private static DataOutputStream out;
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         try {
-            clientSocket = new Socket("localhost", 4050);
-            in = new DataInputStream(clientSocket.getInputStream());
-            out = new DataOutputStream(clientSocket.getOutputStream());
-            System.out.println("Вы подключились к серверу");
-            System.out.println("(Для закрытия соединения введите exit)");
-
-            Runnable readMessage = () -> {
-                try {
-                    while (true) {
-                        String wordFromServer = in.readUTF();//чтение с сервера
-                        System.out.println(wordFromServer);//печать ответа сервера
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            };
-            new Thread(readMessage).start();
-
-            Runnable writeMessage = () -> {
-                try {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//чтение с консоли
-                    while (true) {
-                        System.out.print(": ");
-                        String messageFromClient = reader.readLine();
-                        if ("exit".equalsIgnoreCase(messageFromClient)) {
-                            break;
-                        }
-                        out.writeUTF(messageFromClient);
-                        out.flush();
-                    }
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                } finally {
+            try {
+                clientSocket = new Socket("localhost", 4050);
+                in = new DataInputStream(clientSocket.getInputStream());
+                out = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//чтение с консоли
+                System.out.println("Вы подключились к серверу");
+                System.out.println("(Для закрытия соединения введите exit)");
+                Runnable readMessage = () -> {//чтение с сервера будет крутиться в отдельном потоке
                     try {
-                        closeSocket();
+                        while (true) {
+                            String wordFromServer = in.readUTF();//чтение с сервера
+                            System.out.println(wordFromServer);//печать ответа сервера
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                };
+                new Thread(readMessage).start();
+                while (true) {//чтение с консоли и отправка сообщений на сервре - в потоке main
+                    System.out.print(": ");
+                    String messageFromClient = reader.readLine();
+                    if ("exit".equalsIgnoreCase(messageFromClient)) {
+                        break;
+                    }
+                    out.writeUTF(messageFromClient);
+                    out.flush();
                 }
-            };
-            new Thread(writeMessage).start();
-
-        } catch (IOException e) {
-            closeSocket();
-        }
-    }
-
-
-    public static void closeSocket() throws IOException {
-        try {
-            if (!clientSocket.isClosed())
-                in.close();//сначала закрываем каналы
-            out.close();
-            clientSocket.close();//потом закрываем сокет
-            System.out.println("Вы отключились");
+            } finally {
+                in.close();//если случился break, сначала закрываем каналы
+                out.close();
+                clientSocket.close();//потом закрываем сокет
+                System.out.println("Вы отключились");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
